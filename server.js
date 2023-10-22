6f6962f62b13f3eb096d04342f3cee7986ee3331
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-10-22 16:08:28 -0400

    feat: Add express server setup

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import 'dotenv/config';
+import { fileURLToPath } from 'url';
+import session from 'express-session';
+import errorHandler from './middleware/errorMw.js';
+
+const port = process.env.PORT || 5000;
+const app = new express();
+
+const viewsPath = fileURLToPath(new URL('views', import.meta.url));
+const staticsPath = fileURLToPath(new URL('public', import.meta.url));
+
+app.set('views', viewsPath);
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.static(staticsPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`App server listening on port ${port}`);
+});
